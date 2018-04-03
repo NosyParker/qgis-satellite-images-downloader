97a2cc8b9ad90c68e54aec18a15855192a00f1bb
Roman <sektor_wins@mail.ru>
2018-04-04 02:52:49 +0500

    Delete sat-search source code

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# API URL
-SAT_API = os.getenv('SAT_API', 'https://api.developmentseed.org/satellites')
-
-# Директория, в которой будут помещаться скачанные снимки
-DATADIR = os.getenv('SATUTILS_DATADIR', os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') )
-
-SUBDIRS = 'satellite_name/scene_id'
