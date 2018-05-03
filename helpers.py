fef8fefc857e5e9f52ccec5e4ad0d92b4d41d15f
Roman <sektor_wins@mail.ru>
2018-05-04 03:07:37 +0500

    Добавлен новый модуль - helpers, содержащий вспомогательные инструменты-классы для обеспечения работы основного модуля. Добавлен клаcc CaptureCoordinates: позволяет фиксировать координаты по щелчку и выводить их в WGS84 вне зависимость от CRS проекта/слоя и пр.

---
+++ helpers.py
@@ -0,0 +1,26 @@
+from PyQt5.QtCore import *
+from qgis.gui import *
+from qgis.utils import *
+from qgis.core import*
+
+class CaptureCoordinates(QgsMapTool):
+    def __init__(self, canvas, textEdit, layer=None, source_crs=None, destination_crs=None):
+        QgsMapTool.__init__(self, canvas)
+        self.canvas = canvas
+        self.textEdit = textEdit
+        self.layer = layer
+        self.setCursor(Qt.CrossCursor)
+        self.source_crs = source_crs
+        self.destination_crs = destination_crs
+
+    def transformCRS(self):
+        return QgsCoordinateTransform(QgsCoordinateReferenceSystem(self.source_crs),
+                    QgsCoordinateReferenceSystem(self.destination_crs), 
+                    QgsProject.instance())
+
+
+    def canvasReleaseEvent(self, event):
+        transform = self.transformCRS()
+        point = self.toLayerCoordinates(self.layer, event.pos())
+        tr_point = transform.transform(point)
+        print ([round(tr_point.x(), 7), round(tr_point.y(), 7)])
