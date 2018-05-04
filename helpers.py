b1fa33879db539ea3a0055b7b52c26de4fd5f02a
Roman Vasilyev <sektor_wins@mail.ru>
2018-05-05 02:12:39 +0500

    Изменен: - класс CaptureCoordinates: добавлена логика работы с AOI_COORDINATES - глобальным листом, хранящим координаты области интереса.

--- helpers.py
+++ helpers.py
@@ -3,6 +3,8 @@ from qgis.gui import *
 from qgis.utils import *
 from qgis.core import*
 
+from .globals import AOI_COORDINATES
+
 class CaptureCoordinates(QgsMapTool):
     def __init__(self, canvas, textEdit, layer=None, source_crs=None, destination_crs=None):
         QgsMapTool.__init__(self, canvas)
@@ -23,4 +25,4 @@ class CaptureCoordinates(QgsMapTool):
         transform = self.transformCRS()
         point = self.toLayerCoordinates(self.layer, event.pos())
         tr_point = transform.transform(point)
-        print ([round(tr_point.x(), 7), round(tr_point.y(), 7)])
+        AOI_COORDINATES.append([round(tr_point.x(), 7), round(tr_point.y(), 7)])
