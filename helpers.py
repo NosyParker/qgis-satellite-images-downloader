ca7849e1b54a545ab3b39bc6d18441f9504ad50b
Roman Vasilyev <sektor_wins@mail.ru>
2018-05-07 02:22:18 +0500

    Добавлен функционал для класса CaptureCoordinates: - теперь отображается полигон красного цвета из вершин, которые кликает юзер: - по ЛКМ ставится точка и дорисовывается полигон, по ПКМ - ресетится полигон: - дабл-ЛКМ - отключает инструмент по захвату координат.

--- helpers.py
+++ helpers.py
@@ -1,19 +1,30 @@
+from PyQt5.QtGui import QColor
 from PyQt5.QtCore import *
 from qgis.gui import *
 from qgis.utils import *
-from qgis.core import*
+from qgis.core import *
 
 from .globals import AOI_COORDINATES
 
-class CaptureCoordinates(QgsMapTool):
+class CaptureCoordinates(QgsMapToolEmitPoint):
+    canvasDoubleClicked = QtCore.pyqtSignal(object, object)
+
     def __init__(self, canvas, textEdit, layer=None, source_crs=None, destination_crs=None):
-        QgsMapTool.__init__(self, canvas)
+        QgsMapToolEmitPoint.__init__(self, canvas)
         self.canvas = canvas
         self.textEdit = textEdit
         self.layer = layer
         self.setCursor(Qt.CrossCursor)
         self.source_crs = source_crs
         self.destination_crs = destination_crs
+        self.rubberBand = QgsRubberBand(self.canvas, QgsWkbTypes.geometryType(QgsWkbTypes.Polygon))
+        self.rubberBand.setColor(Qt.red)
+        self.rubberBand.setWidth(1)
+        self.reset()
+
+    def reset(self):
+        self.rubberBand.reset(QgsWkbTypes.geometryType(QgsWkbTypes.Polygon))
+
 
     def transformCRS(self):
         return QgsCoordinateTransform(QgsCoordinateReferenceSystem(self.source_crs),
@@ -21,8 +32,18 @@ class CaptureCoordinates(QgsMapTool):
                     QgsProject.instance())
 
 
-    def canvasReleaseEvent(self, event):
-        transform = self.transformCRS()
-        point = self.toLayerCoordinates(self.layer, event.pos())
-        tr_point = transform.transform(point)
-        AOI_COORDINATES.append([round(tr_point.x(), 7), round(tr_point.y(), 7)])
+    def canvasPressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            transform = self.transformCRS()
+            point = self.toLayerCoordinates(self.layer, event.pos())
+            self.rubberBand.addPoint(point,True)
+            self.rubberBand.show()
+            tr_point = transform.transform(point)
+            AOI_COORDINATES.append([round(tr_point.x(), 7), round(tr_point.y(), 7)])
+        elif event.button() == Qt.RightButton:
+            self.reset()
+
+
+    def canvasDoubleClickEvent(self, event):
+        self.canvas.unsetMapTool(self)
+
